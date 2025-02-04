24de9fa4bbb9ec902ba04e4e5603f89920bd2afa
Qwerty21o <salimgireyevalisher@gmail.com>
2025-02-05 03:31:35 +0500

    step 14

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
-import ItemStatusFilter from "./components/item-status-filter";
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
+
+import './index.css';
 
 const App = () => {
 
     const todoData = [
-        { label: 'Drink Coffee', important: false,  id: 1},
-        { label: 'Implement React Application', important: true, id: 2 },
-        { label: 'Make notes from your stud', important: false, id: 3 },
-        { label: 'Do not drink alchohol', important: false, id: 4 }
-    ];
+    { label: 'Drink Coffee', important: false,  id: 1},
+    { label: 'React Application', important: true, id: 2 },
+    { label: 'Make notes from your stud', important: false, id: 3 },
+    { label: 'Do not drink alchohol', important: false, id: 4 }
+];
     
     return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList todos =  {todoData} />
-            <ItemStatusFilter />
+        <div className="todo-app">
+            <AppHeader toDo={1} done={3} />
+            <div className="top-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
+            </div>
+            
+            <TodoList todos={todoData} />
         </div>
     );
 }
@@ -31,3 +36,9 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') 
 )
 root.render(<App />)
+// const todoData = [
+//     { label: 'Drink Coffee', important: false,  id: 1},
+//     { label: 'Implement React Application', important: true, id: 2 },
+//     { label: 'Make notes from your stud', important: false, id: 3 },
+//     { label: 'Do not drink alchohol', important: false, id: 4 },
+// ];
