38514a399507a439c664a4d33d75126b5fd4f4aa
Qwerty21o <salimgireyevalisher@gmail.com>
2025-02-14 18:54:18 +0500

    step 17

--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
-import ItemStatusFilter from './components/item-status-filter';
-
-import './index.css';
-
-const App = () => {
-
-    const todoData = [
-    { label: 'Drink Coffee', important: false,  id: 1},
-    { label: 'React Application', important: true, id: 2 },
-    { label: 'Make notes from your stud', important: false, id: 3 },
-    { label: 'Do not drink alchohol', important: false, id: 4 }
-];
-    
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
-            </div>
-            
-            <TodoList todos={todoData} />
-        </div>
-    );
-}
+import App from './components/app';
 
 
     
@@ -36,9 +9,3 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') 
 )
 root.render(<App />)
-// const todoData = [
-//     { label: 'Drink Coffee', important: false,  id: 1},
-//     { label: 'Implement React Application', important: true, id: 2 },
-//     { label: 'Make notes from your stud', important: false, id: 3 },
-//     { label: 'Do not drink alchohol', important: false, id: 4 },
-// ];
