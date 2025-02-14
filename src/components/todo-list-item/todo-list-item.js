c696813780839b37da7b8f6285a4a328e87d478a
Qwerty21o <salimgireyevalisher@gmail.com>
2025-02-14 19:20:19 +0500

    step 22

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,7 +4,8 @@ import './todo-list-item.css';
 export default class TodoListItem extends Component {
  
   state = {
-    done: false
+    done: false,
+    important: false
 };
   
   onLabelClick = () => {
@@ -12,25 +13,29 @@ export default class TodoListItem extends Component {
       done: true
   })
     };
-  
-    render () {
+    onMarkImportant = () => {
+      this.setState( {
+          important: true
+      })
+}
     
-    const { label, important = false } = this.props;
-    const { done } = this.state;
+render () {
+    
+  const { label } = this.props;
+  const { done, important } = this.state;
         
     let classNames = 'todo-list-item';
     if (done) {
         classNames += ' done';
     }
-    const style = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
-    };
+    if (important) {
+      classNames += ' important';
+  }
     return (
         <span className={classNames}>
         <span
             className="todo-list-item-label"
-            style={style}
+            
             onClick={ this.onLabelClick }>
         {label}
       </span>
@@ -39,7 +44,8 @@ export default class TodoListItem extends Component {
         <i className="fa fa-exclamation" />
       </button>
       <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
+               className="btn btn-outline-success btn-sm float-right"
+               onClick={ this.onMarkImportant }>
         <i className="fa fa-trash-o" />
       </button>
     </span>
